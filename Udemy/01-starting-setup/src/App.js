45412c6a694c030b9fede58cd24b4540a3fbd0e2
Manuel Cerdas <m.cerdas.salazar@accenture.com>
2021-08-03 08:26:21 -0600

    Syncing progress to work in home computer

---
+++ Udemy/01-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseComponent from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseComponent></ExpenseComponent>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ Udemy/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseComponent () {
+    return (<h2>Expense Item</h2>)
+}
+
+export default ExpenseComponent;
